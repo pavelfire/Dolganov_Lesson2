72575b3111daf9161b85c96da9cc685f9b84661f
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-11 22:16:10 +0300

    second xml design work with buttons

--- app/src/main/java/com/vk/directop/dolganov_lesson2/MainActivity.kt
+++ app/src/main/java/com/vk/directop/dolganov_lesson2/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.vk.directop.dolganov_lesson2
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+
+private const val KEY_ONE = "HELLO_KEY"
 
 class MainActivity : AppCompatActivity() {
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                // Handle the Intent https://developer.android.com/training/basics/intents/result
+                val tvInfo1 = findViewById<TextView>(R.id.tvInfo1)
+                if (intent != null) {
+                    val received =
+                        intent.extras?.getString(SecondActivityContract.RESULT_KEY).toString()
+                    if (received != "") {
+                        tvInfo1.text = received
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btSecondActivity = findViewById<Button>(R.id.btSecondActivity)
+        val tvInfo1 = findViewById<TextView>(R.id.tvInfo1)
+
+        btSecondActivity.setOnClickListener {
+            val intent1 = Intent(this, SecondActivity::class.java)
+            intent1.putExtra(
+                KEY_ONE,
+                "${resources.getString(R.string.textFromMain)}: \n ${tvInfo1.text}"
+            )
+            startForResult.launch(intent1)
+        }
     }
 }
---
+++ app/src/main/java/com/vk/directop/dolganov_lesson2/SecondActivityContract.kt
@@ -0,0 +1,24 @@
+package com.vk.directop.dolganov_lesson2
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+
+class SecondActivityContract : ActivityResultContract<Unit, String?>() {
+
+    override fun parseResult(resultCode: Int, result: Intent?) : String? {
+        if (resultCode != Activity.RESULT_OK) {
+            return null
+        }
+        return result?.getStringExtra(RESULT_KEY)
+    }
+
+    companion object {
+        const val RESULT_KEY = "from_edit_text"
+    }
+
+    override fun createIntent(context: Context, input: Unit): Intent {
+        return Intent(context, SecondActivity::class.java)
+    }
+}
