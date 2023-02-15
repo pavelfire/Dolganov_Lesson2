7e591d6d9397dfe6a2a0ba27f7b538cb52acc6a8
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-15 08:49:24 +0300

    grid and frame better

--- Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/FrameLayoutActivity.kt
+++ Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/FrameLayoutActivity.kt
@@ -2,10 +2,13 @@ package com.vk.directop.dolganov_lesson_3_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class FrameLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame_layout)
+
+        Toast.makeText(this, this.localClassName, Toast.LENGTH_LONG).show()
     }
 }
