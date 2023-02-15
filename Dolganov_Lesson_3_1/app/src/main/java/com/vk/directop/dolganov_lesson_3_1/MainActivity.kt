053eae308fd35bdaed41674c7ddf5124479a82b4
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-15 06:13:51 +0300

    added group, flow, looks better

--- Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/MainActivity.kt
+++ Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/MainActivity.kt
@@ -4,27 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.constraintlayout.widget.Group
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val aLogo = findViewById<ImageView>(R.id.a_logo)
         val btContacts = findViewById<Button>(R.id.bt_contacts)
         val btVacancies = findViewById<Button>(R.id.bt_vacancies)
         val btGallery = findViewById<Button>(R.id.bt_gallery)
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val group = findViewById<Group>(R.id.group)
 
         btContacts.setOnClickListener {
             progressBar.visibility = View.VISIBLE
+            group.visibility = View.INVISIBLE
         }
         btVacancies.setOnClickListener {
             progressBar.visibility = View.VISIBLE
+            group.visibility = View.INVISIBLE
         }
         btGallery.setOnClickListener {
             progressBar.visibility = View.VISIBLE
+            group.visibility = View.INVISIBLE
         }
+
+        aLogo.setOnClickListener {
+            progressBar.visibility = View.INVISIBLE
+            group.visibility = View.VISIBLE
+        }
+
         fun showProgress(){
             progressBar.visibility = View.VISIBLE
 
