5e81e608129126190d6f7fde7d35d529d218f1a9
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-17 09:03:59 +0300

    clock in progress

--- Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/AnalogClockView.kt
+++ Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/AnalogClockView.kt
@@ -4,8 +4,13 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
+import java.util.*
+import kotlin.math.cos
+import kotlin.math.sin
 
 class AnalogClockView
 @JvmOverloads
@@ -13,45 +18,102 @@ constructor(
     context: Context,
     attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    private var padding = 0
+    private var fontSize = 0
+    private val numeralSpacing = 0
+    private var handTruncation = 0
+    private var hourHandTruncation = 0
+    private var radius = 0f
+    lateinit var paint: Paint
+    private var isInit = false
+    private val numbers = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+    private val rect: Rect = Rect()
 
-    val paint = Paint().apply {
-        color = Color.BLACK
-        style = Paint.Style.STROKE
-        strokeWidth = 10f
+
+    private fun initClock() {
+        padding = numeralSpacing + 50
+        fontSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP, 13f,
+            resources.displayMetrics
+        ).toInt()
+        val min = height.coerceAtMost(width)
+        radius = min / 2.2f - padding
+        handTruncation = min / 20
+        hourHandTruncation = min / 7
+        paint = Paint()
+        isInit = true
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+    override fun onDraw(canvas: Canvas) {
+        if (!isInit) {
+            initClock()
+        }
+        canvas.drawColor(Color.WHITE)
+        drawCircle(canvas)
+        drawCenter(canvas)
+        drawClockStroke(canvas)
+        drawHands(canvas)
+        postInvalidateDelayed(500)
+        invalidate()
+    }
 
-        drawClockFace(canvas)
+    private fun drawHand(canvas: Canvas, loc: Double, arrowType: ClockArrow) {
+        val angle = Math.PI * loc / 30 - Math.PI / 2
+        val handRadius  = when (arrowType){
+            ClockArrow.HOUR -> 300f
+            ClockArrow.MINUTE -> 54f
+            ClockArrow.SECOND -> 200f
+        }
+        canvas.drawLine(
+            width / 2f - (cos(angle) * 50).toFloat(),
+            height / 2f - (sin(angle) * 50).toFloat(),
+            (width / 2 + cos(angle) * handRadius).toFloat(),
+            (height / 2 + sin(angle) * handRadius).toFloat(),
+            paint
+        )
+    }
 
-        paint.strokeWidth = 10f
-        canvas?.drawLine(width / 2f, height / 2f, 60f, 50f, paint)
-
+    private fun drawHands(canvas: Canvas) {
+        val c = Calendar.getInstance()
+        var hour = c[Calendar.HOUR_OF_DAY].toFloat()
+        hour = if (hour > 12) hour - 12 else hour
+        drawHand(canvas, ((hour + c[Calendar.MINUTE] / 60) * 5f).toDouble(), ClockArrow.HOUR)
         paint.color = Color.BLUE
-        paint.strokeWidth = 30f
-
-        canvas?.drawLine(300f, 300f, width / 2f, height / 2f, paint)
+        drawHand(canvas, c[Calendar.MINUTE].toDouble(), ClockArrow.MINUTE)
+        paint.color = Color.RED
+        drawHand(canvas, c[Calendar.SECOND].toDouble(), ClockArrow.SECOND)
     }
 
-    private fun drawClockFace(canvas: Canvas?) {
-        val w = (width / 2).toFloat()
-        val h = (height / 2).toFloat()
-        val radius = if (height >= width) width / 2.5f else height / 2.5f
-        val pointLength = 50
+    private fun drawClockStroke(canvas: Canvas) {
+        paint.textSize = fontSize.toFloat()
+        for (number in numbers) {
+            paint.strokeWidth = 20f
+            paint.color =Color.BLACK
 
-        paint.strokeWidth = 20f
-        canvas?.drawCircle(w, h, radius, paint)
-
-        paint.strokeWidth = 20f
-        canvas?.drawLine(w + radius - pointLength, h, w + radius, h, paint)
-        canvas?.drawLine(w - radius + pointLength, h, w - radius, h, paint)
-        canvas?.drawLine(w, h + radius - pointLength, w, h + radius, paint)
-        canvas?.drawLine(w, h - radius + pointLength, w, h - radius, paint)
-
-        //canvas?.drawLine(w, h - radius + pointLength, w, h - radius, paint)
-
-
-
+            val angle = Math.PI / 6 * (number - 3)
+            val x = (width / 2 + cos(angle) * radius).toFloat()
+            val y = (height / 2 + sin(angle) * radius).toFloat()
+            val xEnd = (width / 2 + cos(angle) * (radius + 40)).toFloat()
+            val yEnd = (height / 2 + sin(angle) * (radius + 40)).toFloat()
+            canvas.drawLine(x, y, xEnd, yEnd, paint)
+        }
     }
+
+    private fun drawCenter(canvas: Canvas) {
+        paint.style = Paint.Style.FILL
+        canvas.drawCircle(width / 2f, height / 2f, 12f, paint)
+    }
+
+    private fun drawCircle(canvas: Canvas) {
+        paint.reset()
+        paint.color = Color.BLACK
+        paint.strokeWidth = 20f
+        paint.style = Paint.Style.STROKE
+        paint.isAntiAlias = true
+        canvas.drawCircle(width / 2f, height / 2f, radius + padding - 10f, paint)
+    }
+}
+
+enum class ClockArrow{
+    HOUR, MINUTE, SECOND
 }
