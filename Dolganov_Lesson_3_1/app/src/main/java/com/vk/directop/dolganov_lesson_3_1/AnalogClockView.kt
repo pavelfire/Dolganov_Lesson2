0177b343260b868bfa0ea7bddcfb6d34230f742f
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-17 18:30:51 +0300

    can change hands length and color bn xml

--- Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/AnalogClockView.kt
+++ Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/AnalogClockView.kt
@@ -6,7 +6,6 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.TypedValue
 import android.view.View
 import java.util.*
 import kotlin.math.cos
@@ -17,9 +16,9 @@ class AnalogClockView
 @JvmOverloads
 constructor(
     context: Context,
-    attrs: AttributeSet? = null, defStyleAttr: Int = 0,
-    //defStyleRes: Int
+    attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
     private var padding = 0
     private var fontSize = 0
     private val numeralSpacing = 0
@@ -33,14 +32,12 @@ constructor(
     private var hourColor by Delegates.notNull<Int>()
     private var minuteColor by Delegates.notNull<Int>()
     private var secondColor by Delegates.notNull<Int>()
-
+    private var hourHandLength by Delegates.notNull<Float>()
+    private var minuteHandLength by Delegates.notNull<Float>()
+    private var secondHandLength by Delegates.notNull<Float>()
 
     private fun initClock() {
         padding = numeralSpacing + 50
-        fontSize = TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_SP, 13f,
-            resources.displayMetrics
-        ).toInt()
         val min = height.coerceAtMost(width)
         radius = min / 2.2f - padding
         handTruncation = min / 20
@@ -55,27 +52,27 @@ constructor(
         }
         canvas.drawColor(Color.WHITE)
         drawCircle(canvas)
-        drawCenter(canvas)
         drawClockStroke(canvas)
         drawHands(canvas)
+        //drawCenter(canvas)
         postInvalidateDelayed(500)
         invalidate()
     }
 
     private fun drawHand(canvas: Canvas, loc: Double, arrowType: ClockArrow) {
         val angle = Math.PI * loc / 30 - Math.PI / 2
-        val handLength = height.coerceAtMost(width)
-        val handRadius = when (arrowType) {
-            ClockArrow.HOUR -> handLength * 0.25f
-            ClockArrow.MINUTE -> handLength * 0.3f
-            ClockArrow.SECOND -> handLength * 0.35f
+        val baseLength = height.coerceAtMost(width)
+        val handLength = when (arrowType) {
+            ClockArrow.HOUR -> if (hourHandLength == -1f) baseLength * 0.25f else hourHandLength
+            ClockArrow.MINUTE -> if (minuteHandLength == -1f) baseLength * 0.3f else minuteHandLength
+            ClockArrow.SECOND -> if (secondHandLength == -1f) baseLength * 0.35f else secondHandLength
         }
         val tailHand = 60
         canvas.drawLine(
             width / 2f - (cos(angle) * tailHand).toFloat(),
             height / 2f - (sin(angle) * tailHand).toFloat(),
-            (width / 2 + cos(angle) * handRadius).toFloat(),
-            (height / 2 + sin(angle) * handRadius).toFloat(),
+            (width / 2 + cos(angle) * handLength).toFloat(),
+            (height / 2 + sin(angle) * handLength).toFloat(),
             paint
         )
     }
@@ -110,10 +107,10 @@ constructor(
         }
     }
 
-    private fun drawCenter(canvas: Canvas) {
-        paint.style = Paint.Style.FILL
-        canvas.drawCircle(width / 2f, height / 2f, 12f, paint)
-    }
+//    private fun drawCenter(canvas: Canvas) {
+//        paint.style = Paint.Style.FILL
+//        canvas.drawCircle(width / 2f, height / 2f, 22f, paint)
+//    }
 
     private fun drawCircle(canvas: Canvas) {
         paint.reset()
@@ -126,7 +123,7 @@ constructor(
 
     init {
         if (attrs != null) {
-            initAttributes(attrs, defStyleAttr)//, defStyleRes )
+            initAttributes(attrs, defStyleAttr)
         }
     }
 
@@ -142,6 +139,11 @@ constructor(
         hourColor = typedArray.getColor(R.styleable.AnalogClockView_hourHandColor, Color.MAGENTA)
         minuteColor = typedArray.getColor(R.styleable.AnalogClockView_minuteHandColor, Color.YELLOW)
         secondColor = typedArray.getColor(R.styleable.AnalogClockView_secondHandColor, Color.GREEN)
+        hourHandLength = typedArray.getDimension(R.styleable.AnalogClockView_hourHandLength, -1f)
+        minuteHandLength =
+            typedArray.getDimension(R.styleable.AnalogClockView_minuteHandLength, -1f)
+        secondHandLength =
+            typedArray.getDimension(R.styleable.AnalogClockView_secondHandLength, -1f)
         typedArray.recycle()
     }
 }
