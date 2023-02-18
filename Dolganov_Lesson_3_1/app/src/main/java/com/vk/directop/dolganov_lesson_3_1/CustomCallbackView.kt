84f915a51e612ef298b1dee77109238b0e53a3e8
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-18 10:06:38 +0300

    work on custom button

--- Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/CustomCallbackView.kt
+++ Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/CustomCallbackView.kt
@@ -1,6 +1,24 @@
 package com.vk.directop.dolganov_lesson_3_1
 
+import android.content.Context
+import android.util.AttributeSet
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 
-class CustomCallbackView  {
+class CustomCallbackView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+//    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(
+//        context,
+//        attrs,
+//        defStyleAttr,
+//        0
+//    )
+//
+//    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, 0)
+//    constructor(context: Context) : this(context, null)
 }
