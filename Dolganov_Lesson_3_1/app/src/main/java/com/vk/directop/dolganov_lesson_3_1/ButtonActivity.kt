917f96b03846a03f429e1d377e4e79891207ed61
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-19 21:11:51 +0300

    handler added

--- Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/ButtonActivity.kt
+++ Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/ButtonActivity.kt
@@ -1,7 +1,11 @@
 package com.vk.directop.dolganov_lesson_3_1
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.vk.directop.dolganov_lesson_3_1.databinding.ActivityButtonBinding
 
@@ -9,6 +13,10 @@ class ButtonActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityButtonBinding
 
+    private val token = Any()
+    private val handler = Handler(Looper.getMainLooper())
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,13 +26,22 @@ class ButtonActivity : AppCompatActivity() {
         with(binding) {
 
             mainBtn.setCcvTitle("Android Trainee")
-            mainBtn.setCcvSubtitle("Стажировка по направлению Android")
+
             mainBtn.setCcvImage(getDrawable(R.drawable.a_logo))
 
             mainBtn.setListener {
-                if (it == OnCustomCallbackViewAction.PUSH)
-                    mainBtn.isProgressMode = mainBtn.isProgressMode != true
-                Toast.makeText(this@ButtonActivity, "pressed", Toast.LENGTH_LONG).show()
+                if (it == OnCustomCallbackViewAction.PUSH){
+                    mainBtn.isProgressMode = true
+                    handler.postDelayed({
+                        mainBtn.isProgressMode = false
+                        mainBtn.setCcvSubtitle("Стажировка по направлению Android")
+                        Toast.makeText(this@ButtonActivity, "may be data loaded", Toast.LENGTH_LONG).show()
+                    }, token, 2000)
+
+                }
+
+
+
             }
         }
     }
