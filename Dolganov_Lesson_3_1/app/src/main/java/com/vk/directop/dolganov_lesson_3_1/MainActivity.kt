61ac84e7c75fc7d361342c7d090fbde80b024f3a
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-21 07:44:52 +0300

    vacancies in progress

--- Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/MainActivity.kt
+++ Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/MainActivity.kt
@@ -2,14 +2,49 @@ package com.vk.directop.dolganov_lesson_3_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomMenu : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottomMenu = findViewById(R.id.bottomNavigationView)
+
+        bottomMenu.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.main -> {
+                    val menuFragment = MainFragment()
+                    replaceFragment(menuFragment)
+                }
+                R.id.vacancies -> {
+                    replaceFragment(VacanciesFragment())
+                }
+                R.id.offices -> {
+                    replaceFragment(MainFragment())
+                }
+            }
+            true
+        }
+
+//        supportFragmentManager.beginTransaction()
+//            .add(R.id.fragment_container, LoginFragment.newInstance("w","r"))
+//            .commit()
+
         supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container, LoginFragment.newInstance("w","r"))
+            .add(R.id.fragment_container, VacanciesFragment.newInstance(1))
             .commit()
+
+
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager // обращаемся к fm
+            .beginTransaction() //начать транзакцию
+            .replace(R.id.fragment_container, fragment) // действие
+            .commit() // закончить транзакцию
     }
 }
---
+++ Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/VacancyRecyclerViewAdapter.kt
@@ -0,0 +1,50 @@
+package com.vk.directop.dolganov_lesson_3_1
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.vk.directop.dolganov_lesson_3_1.placeholder.PlaceholderContent.PlaceholderItem
+import com.vk.directop.dolganov_lesson_3_1.databinding.FragmentVacanciesBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class VacancyRecyclerViewAdapter(
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<VacancyRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentVacanciesBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentVacanciesBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
