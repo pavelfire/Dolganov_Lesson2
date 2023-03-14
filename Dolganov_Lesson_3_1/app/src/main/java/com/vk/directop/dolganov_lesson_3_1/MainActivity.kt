07695ae927b4ec30f9c998b885b3448a5b676b23
PavelGL72admdocker <fire_ra@bk.ru>
2023-03-14 10:03:38 +0300

    task conditions

--- Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/MainActivity.kt
+++ Dolganov_Lesson_3_1/app/src/main/java/com/vk/directop/dolganov_lesson_3_1/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.vk.directop.dolganov_lesson_3_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +9,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+/*
+Практическое задание по сервисам (по желанию, кто хочет попробовать поработать с сервисами):
+Создать сервис, который будет каждую секунду выдавать число из последовательности Фибоначчи.
+В развернутом активити число должно отображаться на экране. При закрытом активити должно
+отображаться уведомление, в которое должно приходить очередное число.
+
+Дополнительно: передачу данных из сервиса в активити в этом задании организовать
+через BroadcastReceiver
+ */
